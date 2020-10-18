f0263fe5ea28f8c206cf16f095b7cca27784433f
SebasPelaez <sebaspvilla@hotmail.com>
2020-10-17 22:00:51 -0500

    Nuevos componentes y pruebas

--- src/app/shared/owner/owner.service.spec.ts
+++ src/app/shared/owner/owner.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GiphyService } from './giphy.service';
+import { OwnerService } from './owner.service';
 
-describe('GiphyService', () => {
+describe('OwnerService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: GiphyService = TestBed.get(GiphyService);
+    const service: OwnerService = TestBed.get(OwnerService);
     expect(service).toBeTruthy();
   });
 });
